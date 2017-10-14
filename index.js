6e3d8b16b6473825bcc0315e25f11a402e49c0f9
Kristyn Bryan <kristynrb@gmail.com>
2017-10-14 19:37:51 -0400

    database config & updated view

--- index.js
+++ index.js
@@ -7,13 +7,28 @@ const express = require('express'),
       PORT = process.env.PORT || 3000,
       MONGOURI = process.env.MONGOLAB_URI || 'mongodb://localhost:27017/omni_lab_interview_app'
 
+// VIEW ENGINE
+app.set('views', './views');
+app.set('view engine', 'ejs');
 
-// routes
+// ROUTES
 app.get('/', (req, res) => {
-  res.send("home page");
+  res.render("index.ejs");
 });
 
-// server
-app.listen(3000, () => {
-  console.log("Omni Labs interview app running on port 3000");
+//DATABASE & SERVER
+const db = mongoose.connect(MONGOURI, {
+  useMongoClient: true,
+});
+
+db.on('error', () => {
+  console.log("Database error: check that your mongod instance is running");
+});
+
+db.once('openURI', () => {
+  console.log("Database up and running");
+});
+
+app.listen(PORT, () => {
+  console.log("Omni Labs interview app running on port", PORT);
 });
