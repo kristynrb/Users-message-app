50e557e7bf38e6eebfbe53246b1f6dd3d04648e3
Kristyn Bryan <kristynrb@gmail.com>
2017-10-15 20:36:27 -0400

    sign in working

--- controllers/sessions.js
+++ controllers/sessions.js
@@ -9,9 +9,13 @@ router.get('/new', function(req, res){
 
 //CREATE A NEW SESSION
 router.post('/', function(req, res){
+  console.log(req.body);
     User.findOne({ username: req.body.username }, (err, foundUser) => {
+      console.log(foundUser);
+      console.log(err);
         if(req.body.password == foundUser.password){
             req.session.currentuser = foundUser;
+            req.session.logged = true;
             res.redirect('/welcome');
         } else {
             res.send('wrong password');
