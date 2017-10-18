2fc8c7781dd107bb266b225bad601e7c5570e930
Kristyn Bryan <kristynrb@gmail.com>
2017-10-18 16:45:29 -0400

    updated models to accept org

--- models/messages.js
+++ models/messages.js
@@ -2,8 +2,9 @@ const mongoose = require('mongoose');
 
 const messageSchema = mongoose.Schema ({
     message: {type: String, required: true},
-    status: { type: String },
-    author_id: {type: String}
+    status: { type: String, required: true },
+    author_id: {type: String: required: true},
+    organization: {type: String}
   });
 
 const Message = mongoose.model('Message', messageSchema);
--- models/users.js
+++ models/users.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
       Message = require('./messages.js');
-      
+
 const userSchema = mongoose.Schema ({
     username: {type: String, required: true, unique: true},
     password: {type: String, required: true},
+    organization: {type: String, required: true},
     messages: [Message.schema]
 });
 
